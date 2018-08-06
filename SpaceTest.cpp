486743df53a36b3cf6e6a8751522257d4ec66fa6
tyler <tjfr8s@gmail.com>
2018-08-06 13:57:43 -0700

    Test creation and death of player.

--- SpaceTest.cpp
+++ SpaceTest.cpp
@@ -3,5 +3,10 @@
 
 int main()
 {
+    Player testPlayer(8);
+    std::cout << "is ded: " << testPlayer.isDead() << std::endl;
+    testPlayer.die();
+    std::cout << "is ded: " << testPlayer.isDead() << std::endl;
+
     return 0;
 }
